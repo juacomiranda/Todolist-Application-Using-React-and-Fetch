ec74f02a59e7f76b6a0b82cba5358b9400c33431
juacomiranda <jmiranda@live.cl>
2022-08-18 15:48:37 +0000

    iniciando todolist

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,36 @@
-//import react into the bundle
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
+
+
+export default function App () {
+    const [list, setList] = useState ([])
+    return (
+        <div className ="App">
+            <h1>Lista de Tareas</h1>
+            <form
+            type="submit"
+            onSubmit={(event) => {
+                event.preventDefault();
+                console.log(event.target[0].value);
+            }}>
+            <input />
+            </form>
+            {list.map((tarea, position)=>{
+                return <li>{tarea}</li>
+            })} 
+            
+            {/* <input type="text" placeholder="Ingrese una tarea" 
+                onkeypress= {(event) => {
+                    if (event.key ==== "Enter") {
+                        console.log("Presionaste enter");
+                    }
+                }}> */}
+        </div>
+    )
+
+}
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
