829474f99568eb91ad4d6512a5f9f363bdfcaf0e
juacomiranda <jmiranda@live.cl>
2023-06-21 09:25:29 -0400

    get

--- src/js/component/lista.js
+++ src/js/component/lista.js
@@ -1,19 +1,43 @@
 import React, { useEffect, useState, useContext } from "react";
 
 export function Lista () {
-    const [list, setList] = useState([]);
+    const [list, setList] = useState([])
+    const getTask = ()=>{
+        fetch('http://assets.breatheco.de/apis/fake/todos/user/juacomiranda')
+        .then(data=>data.json())
+        .then(response=>setList(response))
+    }
+    useEffect(()=>{
+        getTask();
+    },[])
+    /*const [list, setList] = useState([]);
     const DeleteItems = (indexItem) => {
         setList((prevState) =>
           prevState.filter((elemento, indice) => indice !== indexItem)
         );
-    };
+    };*/
+    
 
     return (
+        <>
+             <form type="submit">
+            <input className="form-control" placeholder="Ingrese una tarea y precione Enter"/>
+            </form>
+
+            {list.map((elem, index)=>{
+                return <li key={index}>{elem.label}
+                <button onClick={() => DeleteItems(index)}>
+                <i className="fas fa-trash-alt" />
+              </button>
+                </li>
+            })}
+        </>
+    )
+    /*return (
         <>
             <form type="submit" onSubmit={(event) => {
                 event.preventDefault();
                setList([...list, event.target[0].value]);
-               console.log(list);
             }}
             >
             <input className="form-control" placeholder="Ingrese una tarea y precione Enter"/>
@@ -27,6 +51,6 @@ export function Lista () {
               </li>;
             })}
         </>
-    )
+    )*/
 
 }
