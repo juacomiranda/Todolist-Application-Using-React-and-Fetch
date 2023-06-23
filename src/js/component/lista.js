1a754420b0444d00d7109a6171459358e61f42e8
juacomiranda <jmiranda@live.cl>
2023-06-23 00:43:06 -0400

    Delete button

--- src/js/component/lista.js
+++ src/js/component/lista.js
@@ -1,35 +1,58 @@
 import React, { useEffect, useState, useContext } from "react";
 
 export function Lista () {
-    const [list, setList] = useState([])
+    const [list, setList] = useState([]);
+    const DeleteItems = (indexItem) => {
+        setList((prevState) =>
+          prevState.filter((elemento, indice) => indice !== indexItem)
+        );
+    };
     const getTask = ()=>{
         fetch('http://assets.breatheco.de/apis/fake/todos/user/juacomiranda')
         .then(data=>data.json())
         .then(response=>setList(response))
     }
+
+    const putTask = (arrTareas)=>{
+        let header = new Headers();
+        header.append('Content-Type', 'aplication/json')
+
+        let cuerpo = JSON.stringify(arrTareas)
+
+        let requestOptions = {
+            method: 'PUT',
+            headers: header,
+            body: cuerpo,
+            redirect: 'follow'
+        }
+
+        fetch('http://assets.breatheco.de/apis/fake/todos/user/juacomiranda', requestOptions)
+        .then(data=>data.JSON())
+        .then(response=>console.log(response))
+}
+
     useEffect(()=>{
         getTask();
     },[])
-    /*const [list, setList] = useState([]);
-    const DeleteItems = (indexItem) => {
-        setList((prevState) =>
-          prevState.filter((elemento, indice) => indice !== indexItem)
-        );
-    };*/
     
+   
 
     return (
         <>
-             <form type="submit">
+            <form type="submit" onSubmit={(event) => {
+                event.preventDefault();
+               setList([...list, event.target[0].value]);
+            }}
+            >
             <input className="form-control" placeholder="Ingrese una tarea y precione Enter"/>
             </form>
 
             {list.map((elem, index)=>{
                 return <li key={index}>{elem.label}
-                <button onClick={() => DeleteItems(index)}>
-                <i className="fas fa-trash-alt" />
-              </button>
-                </li>
+                            <button onClick={() => DeleteItems(index)}>
+                                <i className="fas fa-trash-alt" />
+                            </button>
+                        </li>;
             })}
         </>
     )
